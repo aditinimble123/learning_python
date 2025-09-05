1fb6286ef0dd9fbc45c71d71d8c5505f6c23afd1
Aditi Nimble <aditinimble@gmail.com>
2025-09-05 23:26:37 +0530

    Add files via upload

---
+++ dict_practiceque.py
@@ -0,0 +1,22 @@
+#store word and there meanings
+dict = {"table":["a piece of furniture" , "list of facts and figures"], # here one key and two values hence stored in the form of tuple
+        "cat": "a small animal"
+    }
+print(dict)
+
+# Store sub and find how many class room req for sub
+
+set = {"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(set)
+print(len(set))
+
+
+
+
+
+
+
+
+
+
+
