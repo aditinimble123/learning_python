552d9487b3652baa2269c3093f4cd6fd41bb73c8
Aditi Nimble <aditinimble@gmail.com>
2025-09-05 23:34:01 +0530

    Add files via upload

---
+++ practice_que.py
@@ -0,0 +1,26 @@
+# WAP to enter marks of 3 sub from the user and store them in a dict 
+marks = {}
+
+x = int(input("Enter Phy :"))
+marks.update({"phy":x})
+
+x = int(input("Enter chem :"))
+marks.update({"chem":x})
+
+x = int(input("Enter math :"))
+marks.update({"math":x})
+
+print(marks)
+
+
+
+
+
+
+
+
+
+
+
+
+
